0f1fc53e5f05829bb9a5f474e240de99c559ab82
TiagoCiceri <tiagociceri@gmail.com>
2022-03-24 20:31:54 -0300

    news buttons

--- src/screens/AddTask.tsx
+++ src/screens/AddTask.tsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, TextInput, Text } from 'react-native'
 import commonStyles from '../commonStyles'
 
+const initialState = { desc: ''}
+
 export default class AddTask extends Component {
+
+    state ={
+        ...initialState
+    }
 
     render() {
         return (
@@ -17,9 +23,26 @@ export default class AddTask extends Component {
                 >
                     <View style={styles.background}></View>
                 </TouchableWithoutFeedback>
+
                 <View style={styles.container}>
-                    <Text style={styles.header}> Tarefa </Text>
+                    <Text style={styles.header}> Nova Tarefa </Text>
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder=" Informe a descrição..."
+                        onChangeText={desc => this.setState({ desc})}
+                        value={this.state.desc}
+                    />
+
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
+
                 <TouchableWithoutFeedback
                     onPress={this.props.onCancel}
                 >
@@ -36,7 +59,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0, 0, 0, 0.7)',
     },
     container: {
-        flex: 1,
+        //flex: 1,
         backgroundColor: '#FFF',
     },
     header: {
@@ -46,5 +69,24 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         padding: 15,
         fontSize: 20,
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today,
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        width: '90%',
+        height: 40,
+        margin: 15,        
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6,
     }
 })
