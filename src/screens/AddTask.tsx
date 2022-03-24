b5e08f1bc1ccc64744cdbdab6ce62d9c26e568e9
TiagoCiceri <tiagociceri@gmail.com>
2022-03-24 20:13:29 -0300

    nova tarefa

--- src/screens/AddTask.tsx
+++ src/screens/AddTask.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
+import commonStyles from '../commonStyles'
 
 export default class AddTask extends Component {
 
@@ -14,9 +15,15 @@ export default class AddTask extends Component {
                 <TouchableWithoutFeedback
                     onPress={this.props.onCancel}
                 >
-                    <View style={styles.background}>
-
-                    </View>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}> Tarefa </Text>
+                </View>
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}
+                >
+                    <View style={styles.background}></View>
                 </TouchableWithoutFeedback>
             </Modal>
         )
@@ -27,5 +34,17 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 20,
     }
 })
