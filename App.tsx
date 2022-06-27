60dd1e25e8beb07155e316af3e36ee5f04639e8e
TiagoCiceri <tiagociceri@gmail.com>
2022-06-27 13:24:05 -0300

    Navegador

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
-import TaskList from './src/screens/TaskList'
+//import TaskList from './src/screens/TaskList'
+//import Auth from './src/screens/Auth'
+import Navigator from './src/Navigator'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TaskList />
+      <Navigator />
     </View>
   )
 }
