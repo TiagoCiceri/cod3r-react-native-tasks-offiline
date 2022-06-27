1ffc88756bd43e437ff5bc7b47c5c798ea387f94
TiagoCiceri <tiagociceri@gmail.com>
2022-06-27 19:33:29 -0300

    cadastrando

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -1,24 +1,59 @@
 import React, { Component } from 'react'
 import { ImageBackground, Text, StyleSheet, View, TextInput, TouchableOpacity, Platform, Alert } from 'react-native'
+import axios from 'axios'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
+import AuthInput from '../components/Authinput'
+import { server, showError, showSuccess } from '../common'
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    stageNew: false
+}
 
 export default class Auth extends Component {
     
     state = {
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-        stageNew: false
+        ...initialState
     }
 
     signinOrSingup = () => {
         if (this.state.stageNew) {
-            Alert.alert('Sucesso!','Criar conta...')
+            this.signup()
         }else{
             Alert.alert('Sucesso!','Logar')
+        }
+    }
+
+    signup = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword,
+            })
+
+            showSuccess('Usuário cadastro!')
+            this.setState({ ...initialState })
+        } catch (e) {
+            showError(e)
+        }
+    }
+
+    testeBackEnd = async () => {
+        try {
+            Alert.alert('Sucesso!','teste backend')    
+            await axios.get(`${server}/teste`).then((res) => {
+                showSuccess('Teste com backend executado com sucesso! '+res.data)
+            })
+                       
+        } catch(e) {
+            showError(e)
         }
     }
 
@@ -31,18 +66,18 @@ export default class Auth extends Component {
                         {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados' }
                     </Text>
                     {this.state.stageNew && 
-                        <TextInput 
+                        <AuthInput icon='user' 
                             placeholder='Nome' 
                             value={this.state.name} 
                             style={styles.input}                              
                             onChangeText={name => this.setState({ name })} />                   
                     }
-                    <TextInput 
+                    <AuthInput icon='at'
                         placeholder='E-mail' 
                         value={this.state.email} 
                         style={styles.input}  
                         onChangeText={email => this.setState({ email })} />
-                    <TextInput 
+                    <AuthInput icon='lock' 
                         placeholder='Senha' 
                         value={this.state.password} 
                         style={styles.input}  
@@ -50,7 +85,7 @@ export default class Auth extends Component {
                         onChangeText={password => this.setState({ password })} />
 
                     {this.state.stageNew && 
-                        <TextInput 
+                        <AuthInput icon='lock' 
                             placeholder='Confirme a Senha' 
                             value={this.state.confirmPassword} 
                             style={styles.input}  
@@ -72,6 +107,13 @@ export default class Auth extends Component {
                     onPress={() => this.setState({ stageNew: !this.state.stageNew })}>
                         <Text style={styles.buttonText}>
                         {this.state.stageNew ? 'Já possui conta?' : 'Ainda não possui conta?' }
+                        </Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={{ padding: 15 }}
+                    onPress={this.testeBackEnd}>
+                        <Text style={styles.buttonText}>
+                        Teste requisição backend
                         </Text>
                 </TouchableOpacity>
             </ImageBackground>
@@ -114,6 +156,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
         padding: 10,
         alignItems: 'center',
+        borderRadius: 8,
     },
     buttonText: {
         fontFamily: commonStyles.fontFamily,
